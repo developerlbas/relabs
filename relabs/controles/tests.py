9cde8e13a991017e81f60bbd3737643067482ac4
developerlabs <dev.relabs@gmail.com>
2013-08-28 15:45:41 -0500

    Adding more test

--- relabs/controles/tests.py
+++ relabs/controles/tests.py
@@ -1,3 +1,4 @@
+#encoding:utf-8
 """
 Control(es) la parte medular del proyecto, tiene como objetivo 
 determinar las incidencias de cada uno de los trabajadores re-
@@ -16,8 +17,11 @@ from controles.models import Control
 from controles.models import Checada
 from controles.models import Semana
 from controles.models import TipoChecada
+from controles.models import Horario
+from controles.models import Operacion
+from django.contrib.auth.models import User
 
-class TestDetectarIncidencias(TestCase):
+class TestValidarDatos(TestCase):
 	
 	def setUp(self):
 		# variable
@@ -30,12 +34,42 @@ class TestDetectarIncidencias(TestCase):
 		self.control = Control.objects.create(clave_trabajador=self.clave_de_trabajador,rfc=self.rfc,atributo=self.atributo, turno=self.turno)
 		#Dias de la semana (dia, descr)
 		Semana.objects.create(dia=0, descr="Dom")
-		Semana.objects.create(dia=1, descr="Lun")
+		self.lunes =Semana.objects.create(dia=1, descr="Lun")
 		Semana.objects.create(dia=2, descr="Mar")
 		Semana.objects.create(dia=3, descr="Mie")
 		Semana.objects.create(dia=4, descr="Jue")
 		Semana.objects.create(dia=5, descr="Vie")
 		Semana.objects.create(dia=6, descr="Sab")
+		
+		self.tipo_checada = TipoChecada.objects.create(tipo=1, descr="MATUTINO")
+		#"""
+		#clave_trabajador 	= models.ForeignKey(Control)
+		#dia_de_semana		= models.ForeignKey(Semana)
+		#hora				= models.TimeField()
+		#habilitado			= models.SmallIntegerField()
+		#tipo_checada		= models.ForeignKey(TipoChecada)
+		#id					= models.AutoField(primary_key=True)
+		#"""
+		self.horario = Horario.objects.create(clave_trabajador = self.control,
+			dia_de_semana=self.lunes, hora = datetime.today(), 
+			habilitado =1, tipo_checada = self.tipo_checada)
+		
+		self.falta = Operacion.objects.create(operacion=7230, short_descr="F", long_descr="FALTA", grupo=1, jerarquia=1, habilitado=1)
+		#
+		#clave_trabajador 	= models.ForeignKey(Control)
+		#fecha_control		= models.DateField()
+		#hora_control		= models.TimeField()
+		#observaciones		= models.CharField(max_length=200)
+		#habilitado			= models.BooleanField()
+		#operacion			= models.ForeignKey(Operacion)
+		#usuario				= models.ForeignKey(User)
+		#id					= models.AutoField(primary_key=True)
+		self.usuario = User.objects.create(username="iesus")
+		self.trabajador = Checada.objects.create(clave_trabajador=self.control, fecha_control=datetime.today(),
+			hora_control=datetime.today(),
+			observaciones="Primer Intento",
+			habilitado=1,
+			operacion = self.falta, usuario=self.usuario)
 		
 	def test_existen_datos_en_tabla_atributo(self):
 		"""
@@ -93,6 +127,29 @@ class TestDetectarIncidencias(TestCase):
 			print(e)
 			self.assertEqual(0,1)
 			
+	def test_existen_datos_en_tabla_horarios(self):
+		"""
+		Horarios de cada trabajador (al menos 6 dias * e/s = 12 registros)
+		"""
+		try:
+			obj = Horario.objects.select_related().get(clave_trabajador = self.clave_de_trabajador)
+			print("horarios",obj)
+		except ObjectDoesNotExist,e:
+			print(e)
+			self.assertEqual(1,0)
+	#
+	def test_existen_datos_en_tabla_operacion(self):
+		"""
+		La tabla operacion debera contener informacion
+		respecto a la aplicacion de incidencias
+		"""
+		try:
+			obj = Operacion.objects.get(operacion=7230)
+			print("operacion", obj)
+		except ObjectDoesNotExist, e:
+			print(e)
+			self.assertEqual(1,0)
+			
 	def test_existen_datos_en_tabla_checada(self):
 		"""
 		Al menos debe existir datos en la tabla checada
@@ -100,6 +157,24 @@ class TestDetectarIncidencias(TestCase):
 		try:
 			obj = Checada.objects.select_related().get(clave_trabajador=self.clave_de_trabajador)
 			print("checada",obj)
+		except ObjectDoesNotExist, e:
+			print("error_en_checada",e)
+			self.assertEqual(1,0)
+			
+class TestDetectarIncidencias(TestCase):
+	"""
+	Determina si el trabajador X tienen alguna incidencia.
+	Una incidencia puede ser un R- (retardo menor), o bien
+	una excepcion (Festivo , F) o vacaciones (VP2013)
+	"""
+	def test_hay_datos(self):
+		"""
+		Existe informacion de al menos un trabajador en la
+		base de datos
+		"""
+		try:
+			obj = Checada.objects.all()
+			print("Verificando que la información este alamacenada")
 		except ObjectDoesNotExist, e:
 			print(e)
 			self.assertEqual(1,0)
